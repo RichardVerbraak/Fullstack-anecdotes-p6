36e6c479c421ed5cf8632bcab84f2cce0814b035
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-05-20 16:47:57 +0200

    Added working vote function in reducer

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import Anecdotes from './components/Anecdotes'
+import Form from './components/Form'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+	const anecdotes = useSelector((state) => state)
+	const dispatch = useDispatch()
 
-  const vote = (id) => {
-    console.log('vote', id)
-  }
+	const vote = (id) => {
+		dispatch({
+			type: 'VOTE',
+			payload: id,
+		})
+	}
 
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
+	return (
+		<div>
+			<h2>Anecdotes</h2>
+			{anecdotes && <Anecdotes anecdotes={anecdotes} vote={vote} />}
+
+			<h2>create new</h2>
+			<Form />
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Form = () => {
+	return (
+		<div>
+			<form>
+				<div>
+					<input />
+				</div>
+				<button>create</button>
+			</form>
+		</div>
+	)
+}
+
+export default Form
