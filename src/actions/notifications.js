cab746cf916f3a51b7d1909f62a21af702e2629c
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-05-27 16:11:51 +0200

    Timer now runs the full 5 seconds when voting by clearing out the previous setTimeout before running a new timer

--- src/actions/notifications.js
+++ src/actions/notifications.js
@@ -1,15 +1,18 @@
+// 1. Vote => message sent => set timer to remove after 5
+// 2. Vote another time => remove previous timeout => set timer again
+
 const setNotification = (message, time) => {
 	return async (dispatch) => {
-		dispatch({
-			type: 'SET_NOTIFICATION',
-			payload: message,
-		})
-
-		setTimeout(() => {
+		const id = setTimeout(() => {
 			dispatch({
 				type: 'REMOVE_NOTIFICATION',
 			})
 		}, time * 1000)
+
+		dispatch({
+			type: 'SET_NOTIFICATION',
+			payload: { message, id },
+		})
 	}
 }
 
--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -1,12 +1,19 @@
+// 1. State holds the timeoutID of the 'first' notification
+// 2. When the second notification is set => clear the timer of the first notification BEFORE storing the ID of the second timeOut
+
 const notificationReducer = (
-	state = { message: null, visible: false },
+	state = { message: null, visible: false, timeoutID: null },
 	action
 ) => {
 	switch (action.type) {
 		case 'SET_NOTIFICATION':
+			if (typeof state.timeoutID === 'number') {
+				clearTimeout(state.timeoutID)
+			}
 			return {
-				message: action.payload,
+				message: action.payload.message,
 				visible: true,
+				timeoutID: action.payload.id,
 			}
 
 		case 'REMOVE_NOTIFICATION':
